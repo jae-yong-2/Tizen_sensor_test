845b2d06e4e73dd34b2ee85e38f07875c3bb577b
jae-yong-2 <ljy8972@naver.com>
2021-05-25 16:54:20 +0900

    test code

--- js/main.js
+++ js/main.js
@@ -8,9 +8,8 @@
 //http://tizen.org/feature/sensor.wrist_up			
 //http://tizen.org/feature/sensor.pedometer
 			
-function onchangeCB(sensorData){
+window.onload = function(){
 	var test = document.getElementById("HR");
-	
 	
 //	test.innerHTML=sensorData.lightLevel;
 	var sensor=tizen.systeminfo.getCapability("http://tizen.org/feature/sensor.heart_rate_monitor");
@@ -18,26 +17,4 @@ function onchangeCB(sensorData){
 		test.innerHTML="can";
 	else
 		test.innerHTML="can't";
-}
-function onsuccessCB() {
-    console.log('Light sensor service has started successfully.');
-}
-
-
-function init(){
-	document.addEventListener('tizenhwkey', function(e) {
-      if(e.keyName == "back")
-	try {
-	    tizen.application.getCurrentApplication().exit();
-	} catch (ignore) {
-	}
-  });
-	var lightSensor = tizen.sensorservice.getDefaultSensor("LIGHT");
-	lightSensor.setChangeListener(onchangeCB);
-	lightSensor.start(onsuccessCB);
-	
-	
-	
-}
-
-window.onload = init();
+};
